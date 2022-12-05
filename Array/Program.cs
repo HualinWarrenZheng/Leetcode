4b8137b686ac859c66d66475d936250803cc4f2e
Warren Zheng <workforwarrenzheng@gmail.com>
2022-12-05 15:06:49 +1300

    Top 100 liked questions

---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Easy easy = new();
+            Medium medium = new();
+            Hard hard = new();
+
+            easy.Run();
+            medium.Run();
+            hard.Run();
+        }
+    }
+}
---
+++ Top_100_Liked_Questions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Top_100_Liked_Questions
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+        //20. Valid Parentheses
+        public bool IsValid(string s)
+        {
+
+        }
+
+        //1. Two Sum
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[2];
+        }
+    }
+}
